905d2901949f888aa3322841c3c13f1ab1ba767b
Andrey Zemlyachev <kvintilian@gmail.com>
2021-01-03 19:55:28 +0300

    Update DemoApplication.java

--- src/main/java/ru/nelogy/DemoApplication.java
+++ src/main/java/ru/nelogy/DemoApplication.java
@@ -9,7 +9,6 @@ public class DemoApplication {
 
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(DemoApplication.class, args);
-//    System.out.println(context.getBean("profile"));
   }
 
 }
